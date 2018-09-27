984f18818f92a265b7dae33c94429c358a6fb2c1
Daniel Egan <danielegan@Daniels-MacBook-Pro-2.local>
2018-09-27 14:57:03 -0700

    initial commit

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../ahackbot.zip');
+var kuduApi = 'https://ahackbot.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$ahackbot';
+var password = '1SJAPLqBZnLbCE8zq1dtQmoH2L9PTePexbbfaLWZkeg2Ye3z8X7Mc5XNxoR2';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('ahackbot publish');
+  } else {
+    console.error('failed to publish ahackbot', err);
+  }
+});
